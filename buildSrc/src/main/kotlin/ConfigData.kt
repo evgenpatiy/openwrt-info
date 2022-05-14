4bfdd8ac4f2e9e67c0866f349e2cadbe4562cd08
Yevgen Patiy <evgenpatiy@gmail.com>
2022-05-14 13:23:26 +0300

    add dependencies for hilt

---
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -0,0 +1,9 @@
+object ConfigData {
+    const val applicationId = "com.gmail.evgenpatiy.openwrtinfo"
+    const val compileSdkVersion = 32
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 32
+    const val versionCode = 1
+    const val versionName = "0.0.1"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,15 @@
+object Versions {
+
+    const val composeVersion = "1.2.0-beta01"
+    const val hiltVersion = "2.41"
+    const val hiltComposeNavigationVersion = "1.0.0"
+
+    const val gradlePlugin = "4.2.1"
+    const val kotlin = "1.6.21"
+    const val timber = "4.7.1"
+    const val appCompat = "1.3.0"
+    const val material = "1.3.0"
+    const val constraintLayout = "1.1.3"
+    const val jUnit = "4.12"
+
+}
